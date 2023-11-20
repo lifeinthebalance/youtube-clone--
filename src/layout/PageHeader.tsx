d927f67861e04abbf79f4e9c9e4242d0961c34e1
Dirijabl <sickl3im@gmail.com>
2023-11-20 21:27:14 +0300

    small changes

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -9,7 +9,7 @@ export function PageHeader() {
           <LuMenu className="opacity-50 ne" />
         </button>
         <a href="/">
-          <img src={logo} alt="memetube" className="h-6" />
+          <img src={logo} alt="memetube" className="h-6 bg-primary" />
         </a>
       </div>
       <div></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: colors.neutral[200],
+          hover: colors.neutral[300],
+        },
+      },
+    },
   },
   plugins: [],
 };
