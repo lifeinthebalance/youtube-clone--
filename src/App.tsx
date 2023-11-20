92986f6d1d86ed6d84cc452995db793dd9dfe41f
Dirijabl <sickl3im@gmail.com>
2023-11-20 18:39:18 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { PageHeader } from "./layout/PageHeader";
+
+function App() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div>1</div>
+      <div>2</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/PageHeader.tsx
@@ -0,0 +1,19 @@
+import logo from "../assets/download1.png";
+import { LuMenu } from "react-icons/lu";
+
+export function PageHeader() {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <button>
+          <LuMenu className="opacity-50 ne" />
+        </button>
+        <a href="/">
+          <img src={logo} alt="memetube" className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
