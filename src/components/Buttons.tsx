dc859fcbff75dd4a2dbc6a03ad809c8e0e95f3f2
blackrain313 <sickl3im@gmail.com>
2023-11-26 18:10:28 +0300

    commit

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,7 +1,20 @@
-const buttonVariants = {
-  menu: "hover:bg-neutral-100 p-2 w-10 h-10 flex justify-center items-center",
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = {
+  variants: {
+    menuButton:
+      "hover:bg-neutral-100 w-8 h-8 flex justify-center items-center rounded-full",
+  },
 };
 
-export function LightButton({ ...props }) {
-  return <button {...props} className={buttonVariants.menu} />;
+type ButtonProps = ComponentProps<"button">;
+
+export function MenuButton({ className, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={twMerge(buttonStyles.variants.menuButton, className)}
+    />
+  );
 }
--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -1,20 +1,30 @@
 import logo from "../assets/download1.png";
-import { LuMenu } from "react-icons/lu";
-import { LightButton } from "../components/Buttons";
+import { LuBell, LuMenu, LuUpload, LuUser } from "react-icons/lu";
+import { MenuButton } from "../components/Buttons";
 
 export function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
+    <div className="flex gap-10 lg:gap-20 justify-between my-4 mx-6">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <LightButton>
+        <MenuButton>
           <LuMenu />
-        </LightButton>
+        </MenuButton>
         <a href="/">
           <img src={logo} alt="memetube" className="h-6 bg-primary" />
         </a>
       </div>
       <div></div>
-      <div></div>
+      <div className="flex gap-6 mr-8 items-center flex-shrink-0">
+        <button>
+          <LuBell></LuBell>
+        </button>
+        <button>
+          <LuUpload></LuUpload>
+        </button>
+        <button>
+          <LuUser></LuUser>
+        </button>
+      </div>
     </div>
   );
 }
